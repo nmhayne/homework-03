945cc5f8027f8fcd63d63652f77e5546ec849f4c
noah.hayne@gmail.com <noah.hayne@gmail.com>
2020-04-05 18:03:30 -0400

    more progress, taking a break

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,28 @@
+
+// User clicks button
+// Prompt the length of password and save it to the variable passLength 
+// ask which characters together no spaces and save it to variable pickedChars
+// confirmCriteria returns a boolean and if true include it in criteria generating password
+// Math.random gives random decimals. 
+// generatePassword returns the finalPass
+// writePassword changes the page to display the password created in Generate Password
+
+
+
+
+
+
 // DECLARING VARIABLES
-var criteria=[numbers, letters, special]
-var pickedCrtieria=[];
-var numbers= "0123456789"
+var criteria=[numbersBool, charsBool, uppercaseBool, lowercaseBool];
+var pickedCriteria=[];
 var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var pickedChars;
 var passLength;
-
+var uppercaseBool;
+var lowercaseBool;
+var charsBool;
+var numbersBool;
+var finalPass;
 
 
 // Assignment Code
@@ -19,52 +36,62 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
+// FOR CHOSEN PASSWORD LENGTH, PICK RANDOM CRITERIA AND ADD 
 function generatePassword(){
-  finalPass="Testnothignsomethign please"
+  var finalPass="";
+  for(var i=0;i<passLength;i++){
+    var criteria=randomCriteria();
+    switch (criteria) {
+      case "numbers":
+      finalPass += randomDigit(10);
+      break;
+      
+      case "lowercase":
+      finalPass += randomLower();
+      break;
+      
+      case "uppercase":
+      finalPass += randomUpper();
+      break;
+      
+      case "specialChars":
+      finalPass += randomChar();
+      break;
+      
+      default:
+      break;
+    }
+  }
   return finalPass; 
 }
 
-
-// USER PICKING WHICH CRITERIA TO INCLUDE IN IN PASSWORD
-// function promptCriteria() {
-//   while (picking===true) { 
-//     // ASK USER FOR CRITERA
-//     critChoice=prompt("What criteria would you like to inclue? special characters, numbers, uppercase, lowercase/n Enter one at a time and hit cancel when done");
-//     console.log(critChoice);
-//     // IF NOT IN INDEX OF POSSIBLE CHOICES ASK AGAIN ELSE PUSH ONTO 
-//     if(critChoice===null ||critChoice ===""){
-//       picking=false;
-//       break;
-//     }
-//     if (criteria.indexOf(critChoice)===-1){
-//       alert("I'm sorry this choice was invalid, please try again.(special characters, numbers, uppercase, lowercase)");
-//     }
-//     if (criteria.indexOf(critChoice)>=0 && pickedCrtieria.indexOf(critChoice)!==-1){
-//       alert("This criteria is already included.")
-//     }
-//     if (criteria.indexOf(critChoice)>=0 && pickedCrtieria.indexOf(critChoice)===-1){
-//       pickedCrtieria.push(critChoice);
-//       alert(critChoice+" has been added to included criteria.");
-//     }
-//   }
-// }
-
+// FUNCTION TO ASK YES OR NO TO CRITERIA
 function confirmCriteria() {
-  lowercaseBool=confirm("Would you like lowercase letters included?");
-  uppercaseBool=confirm("Would you like uppercase letters included");
-  numbersBool=confirm("Would you like numbers included?");
-  specialBool=confirm("Would you still like your special characters added?");
   
+  // TRUE OR FALSE LOWERCASE LETTERS. IF TRUE ADD TO PICKED CRITERA
+  lowercaseBool=confirm("Would you like lowercase letters included?");
+  if (lowercaseBool===true) {
+    pickedCriteria.push("lowercase");
+  }
+  
+  // TRUE OR FALSE UPPERCASE LETTERS. IF TRUE ADD TO PICKED CRITERA
+  uppercaseBool=confirm("Would you like uppercase letters included");
+  if (lowercaseBool===true) {
+    pickedCriteria.push("uppercase");
+  }
+  
+  // TRUE OR FALSE NUMBERS. IF TRUE ADD TO PICKED CRITERA
+  numbersBool=confirm("Would you like numbers included?");
+  if (numbersBool===true) {
+    pickedCriteria.push("numbers");
+  }
+  
+  // TRUE OR FALSE SPECIAL CHARACTERS. IF TRUE ADD TO PICKED CRITERA
+  specialBool=confirm("Would you still like your special characters added?");
+  if (specialBool===true) {
+    pickedCriteria.push("specialChars");
+  }
 }
- 
-
-
-
-
-
-
-
 
 // FUNCTION TO GET PASSWORD LENGTH FROM USER
 function promptLength() {
@@ -80,22 +107,45 @@ function promptChars() {
   console.log(pickedChars);
 }
 
-
-promptCriteria();
-// User clicks button
-// Prompt the length of password and save it to the variable passLength 
-// ask which characters together no spaces and save it to variable pickedChars
-// confirmCriteria returns a boolean and if true include in
-// generatePassword returns the finalPass
-// writePassword changes the page to display the password created in Generate Password
-
-
-
-
-
-// for the requested password length, push the last characters on 
-for(var i=0; i<passLength,i++;) {
-  for(var j=0; j<passLength,j++;){
-        
-  }
+// GENERATE RANDOM UPPERCASE LETTER BY RANDOMLY CHOOSING A LETTER FROM THE ARRAY AND RETURNING THE UPPERCASE
+function randomUpper(){
+  var letter=letters[randomDigit(26)];
+  var uppercase=letter.toUpperCase();
+  return uppercase;
 }
+
+// GENERATE RANDOM LOWERCASE LETTER BY RANDOMLY CHOOSING A LETTER FROM THE ARRAY AND RETURNING THE LOWERCASE
+function randomLower(){
+  var letter=letters[randomDigit(26)];
+  var lowercase=letter.toLowerCase();
+  return lowercase;
+}
+// GENERATES A RANDOM NUMBER WITH MAX BEING THE INPUT
+function randomDigit(x) {
+  var rando=Math.floor(Math.random()*x);
+  return rando;
+}
+// PICKS A RANDOM CRITERIA FROM THE USER PICKED LIST BY RANDOMLY GETTING A NUMBER AND USING IT AS AN INDEX
+function randomCriteria() {
+  var pickCritLen=pickedCriteria.length;
+  return pickedCriteria[randomDigit(pickCritLen)-1];  
+}
+
+// PICKS A RANDOM SPECIAL CHARACTER FROM THE INPUT STRING
+function randomChar() {
+  var pickCharLen= pickedChars.length;
+  return pickedChars.charAt(randomDigit(pickCharLen)-1);  
+}
+
+
+
+passLength=10;
+var pickedChars="*&^%$#";
+confirmCriteria();
+console.log(pickedCriteria+":picked criteria");
+writePassword();
+
+
+
+
+
